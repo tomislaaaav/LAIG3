46f3fcd24b7703024df0ba30b9e9fb1588650df8
tomislaaaav <m_botelho8@hotmail.com>
2015-10-26 18:19:47 +0000

    state machine feita

--- WebCGF.XMLReaderExample/reader/data/LinearAnimation.js
+++ WebCGF.XMLReaderExample/reader/data/LinearAnimation.js
@@ -4,6 +4,12 @@ function LinearAnimation(scene,node){
 	this.node = node;
 	this.currTime = 0;
 	this.initTime = 0;
+
+	this.x = 0;
+	this.y = 0;
+	this.z = 0;
+
+	this.controlState = 0;
 }
 
 LinearAnimation.prototype = Object.create(Animation.prototype);
@@ -14,6 +20,8 @@ LinearAnimation.prototype.set= function(controlPoint1, controlPoint2, controlPoi
 	this.controlPoint1 = controlPoint1;
 	this.controlPoint2 = controlPoint2;
 	this.controlPoint3 = controlPoint3;
+
+	this.dist = Math.sqrt(Math.pow(controlPoint2.x, 2)+Math.pow(controlPoint2.y, 2)+Math.pow(controlPoint2.z, 2)) + Math.sqrt(Math.pow((controlPoint3.x - controlPoint2.x), 2) + Math.pow((controlPoint3.y - controlPoint2.y), 2) + Math.pow((controlPoint3.z - controlPoint2.z), 2));
 	this.time = time;
 	
 	this.node = node;
@@ -23,6 +31,7 @@ LinearAnimation.prototype.set= function(controlPoint1, controlPoint2, controlPoi
 
 LinearAnimation.prototype.display= function(parentTexture, parentMaterial, currTime){
 	//Aplicar alterações necessárias para o tempo
+	/*
 	this.currTime = currTime;
 	switch(currTime - this.initTime){
 		case currTime:
@@ -34,6 +43,105 @@ LinearAnimation.prototype.display= function(parentTexture, parentMaterial, currT
 			var deltaT = currTime - this.currTime;
 			break;
 	}
+	*/
+
+	switch(this.controlState){
+		case 0:
+
+		break;
+		case 1:
+		if (this.x == this.controlPoint2.x)
+		{
+			if (this.y == this.controlPoint2.y)
+			{
+				if (this.z != this.controlPoint2.z)
+				{
+					//incrementar z
+				}
+			}
+			else if (this.z == this.controlPoint2.z)
+			{
+				//incrementar y
+			}
+			else
+			{
+				//incrementar y e z
+			}
+		}
+		else if (this.y == this.controlPoint2.y)
+		{
+			if (this.z == this.controlPoint2.z)
+			{
+				//incrementar x
+			}
+			else
+			{
+				//incrementar x e z
+			}
+		}
+		else if (this.z == this.controlPoint2.z)
+		{
+			//incrementar x e y
+		}
+		else
+		{
+			//incrementar x, y, z
+		}
+
+		if (this.x == this.controlPoint2.x && this.y == this.controlPoint2.y && this.z == this.controlPoint2.z)
+		{
+			controlState = 2;
+		}
+		break;
+		case 2:
+		if (this.x == this.controlPoint3.x)
+		{
+			if (this.y == this.controlPoint3.y)
+			{
+				if (this.z != this.controlPoint3.z)
+				{
+					//incrementar z
+				}
+			}
+			else if (this.z == this.controlPoint3.z)
+			{
+				//incrementar y
+			}
+			else
+			{
+				//incrementar y e z
+			}
+		}
+		else if (this.y == this.controlPoint3.y)
+		{
+			if (this.z == this.controlPoint3.z)
+			{
+				//incrementar x
+			}
+			else
+			{
+				//incrementar x e z
+			}
+		}
+		else if (this.z == this.controlPoint3.z)
+		{
+			//incrementar x e y
+		}
+		else
+		{
+			//incrementar x, y, z
+		}
+
+		if (this.x == this.controlPoint3.x && this.y == this.controlPoint3.y && this.z == this.controlPoint3.z)
+		{
+			controlState = 3;
+		}
+		break;
+		case 3:
+		console.log("Animation ended\n");
+		break;
+	}
+
 	
 	//aplicar as transformações consoante as alterações feitas previamente
 	
